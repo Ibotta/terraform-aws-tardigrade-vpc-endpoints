5b4b7fc47108777f7f6053a28edd49b8a4d1b36e
Josh Morrisett <josh.morrisett@ibotta.com>
2024-04-09 13:04:10 -0400

    CLD-246: Eliminate for_each tf plan error on ipn-base

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,20 @@ output "vpc_endpoint_sgs" {
 }
 
 output "vpc_endpoint_interface_services" {
-  value = aws_vpc_endpoint.interface_services
+  # value = aws_vpc_endpoint.interface_services
+  value = {
+    for int_serv in aws_vpc_endpoint.interface_services : int_serv.id => {
+      id                    = int_serv.id
+      service_name          = int_serv.service_name
+      vpc_id                = int_serv.vpc_id
+      private_dns_enabled   = int_serv.private_dns_enabled
+      security_group_ids    = int_serv.security_group_ids
+      subnet_ids            = int_serv.subnet_ids
+      network_interface_ids = int_serv.network_interface_ids
+      dns_entry             = int_serv.dns_entry
+      state                 = int_serv.state
+    }
+  }
 }
 
 output "vpc_endpoint_gateway_services" {
